0f5cbbea121b7e4578cf100a137ba35249aed2d6
habeebtoyosi <oluwo.habeeb@venturegardengroup.com>
2016-07-20 11:06:04 +0100

    Created local governments and state model, and added to admin site

--- eCaretakerWeb/admin.py
+++ eCaretakerWeb/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import State, LGA
+
+class StateModelAdmin(admin.ModelAdmin):
+    list_display_links = None
+    list_display = ['name']
+    list_editable = ['name']
+
+    class meta:
+        model = State
+
+class LGAModelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'stateId']
+    list_display_links = ['name']
+    list_filter = ['stateId']
+    search_fields = ['name', 'stateId']
+
+    class meta:
+        model = LGA
+
+admin.site.register(State, StateModelAdmin)
+admin.site.register(LGA, LGAModelAdmin)
--- eCaretakerWeb/models.py
+++ eCaretakerWeb/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+
+class LGA(models.Model):
+    name = models.CharField(
+        ('Local Government Area'),
+        help_text='Please enter the name of the local government',
+        max_length=30,
+        blank=True,
+    )
+
+    stateId = models.ForeignKey('State', models.CASCADE,
+                                verbose_name='state',
+                                help_text='Please select the local government area state',)
+
+    def __str__(self):
+        return "%s" % self.name
+
+
+class State(models.Model):
+    name = models.CharField(
+        ('name'),
+        unique=True,
+        help_text='Please enter the name of the state',
+        max_length=30,
+        blank=True
+    )
+
+    def __str__(self):
+        return '%s' % self.name
+
+    class meta:
+        verbose_name = 'state',
+        verbose_name_plural = 'states'
