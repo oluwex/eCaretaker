9c05936c422b4183e3c5d8fcec57b43f8d732ffb
= <oluwo.habeeb@venturegardengroup.com>
2016-08-23 18:56:52 +0100

    started logoff

--- eCaretaker/settings.py
+++ eCaretaker/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'eCaretakerWeb',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'eCaretakerWeb',
     'eCaretakerLocations',
     'complaints',
 ]
--- eCaretakerWeb/urls.py
+++ eCaretakerWeb/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import login
+from django.contrib.auth.views import login, logout
 # from django.contrib import admin
 from django.conf.urls import url
 from .views import index, register
@@ -7,5 +7,5 @@ urlpatterns = [
     url(r'^$', index, name='Home'),
     url(r'^login/', login, name='login'),
     url(r'^register/', register, name='register'),
-    url(r'^logoff/', name='logoff'),
+    url(r'^logoff/', logout, name='logoff'),
 ]
