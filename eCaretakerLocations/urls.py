d551d6758c3525715a7cfbc7c24c78978e34d748
= <omotee24@gmail.com>
2016-09-27 18:24:52 +0100

    Update on views

--- eCaretakerLocations/urls.py
+++ eCaretakerLocations/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from .views import index
 
 urlpatterns = [
-    url(r'^$', index, name='index')
+    url(r'^$', index, name='index'),
+    url(r'(?P<type>\w+)/$', search, name='search')
 ]
