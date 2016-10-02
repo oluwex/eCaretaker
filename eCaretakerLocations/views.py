af8fd9c17fca0971e3efde90340856b7232d0cb5
= <oluwo.habeeb@venturegardengroup.com>
2016-10-02 21:24:26 +0100

    Started locations page

--- eCaretakerLocations/views.py
+++ eCaretakerLocations/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def index(request):
     return render(request, 'Locations/index.html')
+
+
+#   Todo: Implement house search by location
+
